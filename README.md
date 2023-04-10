8c9a3925f4e5afc6d020607f25c066e4b4072a90
Olivia Bedford <olivia.traveler.524@gmail.com>
2023-04-09 19:24:55 -0500

    final updates

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # leaflet-challenge
+
+Hello! This is the Module 15 Challenge for Rice University's Data Analytics & Visualization Bootcamp. Woohoo!
+
+In this challenge, we used Javascript to make an API call from the US Geological Survey website. We used USGS's earthquake data to populate a Leaflet map with markers to show where earthquakes happened, their magnitude, and their depth.
+
+The main code body for this challenge is contained in the "leaflet" folder in this repository. To view the map, open the index.html file in Google Chrome. 
+
+The code contained in this file is my original work. I obtained advice and assistance from Rice University's team of instructors and tutors, but the work itself is mine and mine alone.
+
+Psst. Why shouldn't you let a geologist drive your car?
+....because they get hammered and stoned. 
+
+*ba-dum tssss*
--- leaflet/static/js/logic.js
+++ leaflet/static/js/logic.js
@@ -1,3 +1,31 @@
+const LINK = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+function getColor(depth) {
+    if (depth > 50) {
+        return "purple";
+    }
+    if (depth > 40) {
+        return "blue";
+    }
+    if (depth > 30 ) {
+        return "green";
+    }
+    if (depth > 20) {
+        return "yellow";
+    }
+    if (depth > 1) {
+        return "orange";
+    }
+    return "red";
+};
+
+function getRadius(magnitude) {
+    if (magnitude ===0) {
+        return 1;
+    }
+    return magnitude * 4;
+};
+
 let myMap = L.map("map", {
     center: [40.7128, -74.0059],
     zoom: 4
@@ -7,9 +35,39 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+d3.json(LINK).then(function(data) {
+    function circleMarker(feature, latlng) {
+        return L.circleMarker(latlng);
+    }
 
-d3.json(link).then(function(data) {
-    L.geojson(data).addTo(myMap);
+
+    function styleInfo(feature){
+        let depth = feature.geometry.coordinates[2];
+        let style = {
+            opacity: 1,
+            fillOpacity: 0.5,
+            fillColor: getColor(depth),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+        return style;
+    }
+
+
+    function popupMaker(feature, layer) {
+        let mag = feature.properties.mag;
+        let place = feature.properties.place;
+        let html = `Magnitude: ${mag}<br>Place: ${place}`;
+        layer.bindPopup(html);
+    }
+
+
+    L.geoJSON(data, {
+        pointToLayer: circleMarker,
+        style: styleInfo,
+        onEachFeature: popupMaker
+    }).addTo(myMap);
 });
 
